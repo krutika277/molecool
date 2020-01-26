0cdcd4428f7b937e60618fc348c1ead810ce20db
Krutika Patidar <krutikap@buffalo.edu>
2020-01-26 13:01:55 -0500

    changes in measure.py and test_measure.py files

--- molecool/measure.py
+++ molecool/measure.py
@@ -46,4 +46,61 @@ def calculate_angle(rA, rB, rC, degrees=False):
         return np.degrees(theta)
     else:
         return theta
+def calculate_molecular_mass(symbols):
+   """Calculate the mass of a molecule.
+   
+   Parameters
+   ----------
+   symbols : list
+       A list of elements.
+   
+   Returns
+   -------
+   mass : float
+       The mass of the molecule
+   """
+   mass = 0 
+   
+   for atom in symbols:
+       if atom not in atomic_weights.keys():
+           raise ValueError(F"Element {atom} not supported")
+
+       mass += atomic_weights[atom]
+   return mass
+
+   pass
+
+def calculate_center_of_mass(symbols, coordinates):
+   """Calculate the center of mass of a molecule.
+   
+   The center of mass is weighted by each atom's weight.
+   
+   Parameters
+   ----------
+   symbols : list
+       A list of elements for the molecule
+   coordinates : np.ndarray
+       The coordinates of the molecule.
+   
+   Returns
+   -------
+   center_of_mass: np.ndarray
+       The center of mass of the molecule.
+   Notes
+   -----
+   The center of mass is calculated with the formula
+   
+   .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+   
+   """
+   total_mass = calculate_molecular_mass(symbols)
+   mass_array = np.zeros([len(symbols),1])
+
+   for i in range(len(symbols)):
+       mass_array[i] = atomic_weights[symbols]
+
+   center_of_mass = sum(coordinates * mass_array) / total_mass
+
+   return centre_of_mass
+        
 
--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -56,3 +56,23 @@ def test_calculate_angle_many(p1,p2,p3,expected_angle):
     calculated_angle = molecool.calculate_angle(p1, p2, p3, degrees=True)
 
     assert expected_angle == pytest.approx(calculated_angle)
+
+def test_molecular_mass(test_molecule):
+    symbols = ['C', 'H', 'H', 'H', 'H']
+    
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = molecool.atom_data.atomic_weights['C'] + molecool.atom_data.atomic_weights['H'] +\
+         molecool.atom_data.atomic_weights['H'] + molecool.atom_data.atomic_weights['H'] + molecool.atom_data.atomic_weights['H']
+    
+    assert actual_mass == calculated_mass
+
+def test_center_of_mass():
+    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+
+    center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1,1,1])
+
+    assert center_of_mass.all() == expected_center.all()
